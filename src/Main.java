78c8e0e7a1acf7510da399820190627c1009a41e
Zaydaan Jahangir <jahangir.z@northeastern.edu>
2024-11-23 18:31:29 -0500

    Update code to handle command line arguments

--- src/Main.java
+++ src/Main.java
@@ -16,37 +16,57 @@ public class Main {
   /**
    * Main method to run the game and can be played against PvP or PvCPU
    *
-   * @param args args
+   * @param args command line arguments
    */
   public static void main(String[] args) {
+    if (args.length != 2) {
+      // error handling for wrong arguments
+      System.out.println("Usage: java Main <player1Type> <player2Type>");
+      System.out.println("playerType options: human, flipmaxStrategy, cornerStrategy");
+      System.exit(1);
+    }
+
     String gridConfigPath = "resources/grid_configs/grid1.txt";
     String cardConfigPath = "resources/card_configs/cards3.txt";
     Random random = new Random();
-
     GridFileReader gridReader = new GridFileReaderImpl();
     Grid grid = gridReader.readGrid(new File(gridConfigPath));
-
     CardFileReader cardReader = new CardFileReaderImpl();
     List<Card> cards = cardReader.readCards(new File(cardConfigPath));
 
     GameModel model = new ThreeTriosGameModel();
+    PlayerAction playerRed = createPlayer("Red", args[0]);
+    PlayerAction playerBlue = createPlayer("Blue", args[1]);
 
-    PlayerImpl playerRed = new PlayerImpl("Red");
-    AIPlayer playerBlue = new AIPlayer("Blue", new CornerStrategy());
-    model.setPlayers(playerRed, playerBlue);
+    model.setPlayers((Player) playerRed, (Player) playerBlue);
     model.initializeGame(grid, cards, random);
 
-    ThreeTriosViewInterface viewRed = new ThreeTriosView(model, playerRed);
-    ThreeTriosViewInterface viewBlue = new ThreeTriosView(model, playerBlue);
-
+    ThreeTriosViewInterface viewRed = new ThreeTriosView(model, (Player) playerRed);
+    ThreeTriosViewInterface viewBlue = new ThreeTriosView(model, (Player) playerBlue);
     ThreeTriosController controllerRed = new ThreeTriosController(
-            model, playerRed, playerRed, viewRed, viewBlue, playerBlue);
+            model, playerRed, (Player) playerRed, viewRed, viewBlue, (PlayerAction) playerBlue);
 
     ThreeTriosController controllerBlue = new ThreeTriosController(
-            model, playerBlue, playerBlue, viewBlue, viewRed, playerRed);
+            model, playerBlue, (Player) playerBlue, viewBlue, viewRed, (PlayerAction) playerRed);
 
     model.startGame();
     viewRed.setVisible(true);
     viewBlue.setVisible(true);
+  }
+
+  private static PlayerAction createPlayer(String color, String playerType) {
+    switch (playerType.toLowerCase()) {
+      case "human":
+        return new PlayerImpl(color);
+      case "flipmaxstrategy":
+        return new AIPlayer(color, new FlipMaxStrategy());
+      case "cornerstrategy":
+        return new AIPlayer(color, new CornerStrategy());
+      default:
+        System.out.println("Invalid player type: " + playerType);
+        System.out.println("Valid options: human, flipmaxStrategy, cornerStrategy");
+        System.exit(1);
+        return null;
+    }
   }
 }

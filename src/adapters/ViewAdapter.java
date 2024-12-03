35e29ef7aef0e973406f6855f3a72b6e2d6c9eda
Zaydaan Jahangir <jahangir.z@northeastern.edu>
2024-12-03 16:26:00 -0500

    Add adapter package

---
+++ src/adapters/ViewAdapter.java
@@ -0,0 +1,33 @@
+package adapters;
+
+import controller.Features;
+import provider.view.ThreeTriosGUIView;
+import view.ThreeTriosViewInterface;
+
+public class ViewAdapter implements ThreeTriosViewInterface {
+  private final ThreeTriosGUIView providerView;
+  @Override
+  public void addFeatures(Features features) {
+
+  }
+
+  @Override
+  public void updateView() {
+
+  }
+
+  @Override
+  public void showErrorMessage(String message) {
+
+  }
+
+  @Override
+  public void showGameOverMessage(String message) {
+
+  }
+
+  @Override
+  public void setVisible(boolean visible) {
+
+  }
+}
